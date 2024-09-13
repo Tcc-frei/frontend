def4bcfa6eeb46871b5f039e9fcc90b95468d490
Rodrigo Gomes Silva <ra43258559805@cursos.insf>
2024-09-13 09:37:38 -0300

    lado esquerdo finalizado

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,27 @@
-import "./app.scss"
+import "./app.scss";
+
+import imgCadeado from "./assets/logo-key.svg";
+
+import logoRaio from "./assets/logo-thunder.svg";
 
 export function App() {
   return (
     <main className="conteudo-principal">
-      <div className="conteudo-esquerdo"></div>
+      <div className="conteudo-esquerdo">
+        <div className="cabecalho-esquerdo">
+          <img src={logoRaio} alt="Logo de um raio" width={60} />
+        </div>
+
+        <img src={imgCadeado} alt="Logo de um cadeado" width={200} />
+
+        <span className="mensagem">
+          Bem-vindo ao portal administrativo da Elethronos. Seu papel é
+          fundamental para a gestão eficiente.
+        </span>
+      </div>
       <div className="conteudo-direito">
-        <h2>PAINEL DE ADMINISTRADOR</h2>
+        <h3>PAINEL DE ADMINISTRADOR</h3>
       </div>
     </main>
-  )
+  );
 }
