bae4c33e6a77365059d44b079ffc246022bf30d1
Rodrigo Gomes Silva <ra43258559805@cursos.insf>
2024-09-17 11:45:50 -0300

    feat: iniciando modal

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,23 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import "./app.scss";
 
 import { useNavigate } from "react-router-dom";
 
 import { usuarioEstaLogado } from "./service/auth.js";
-import { BiUser } from "react-icons/bi";
+import { BiUser, BiX } from "react-icons/bi";
 import { BsPlus } from "react-icons/bs";
 
 export function App() {
+  const [modalIsOpen, setModalIsOpen] = useState(false);
   const navigate = useNavigate();
+
+  function abrirModal() {
+    setModalIsOpen(true);
+  }
+
+  function fecharModal() {
+    setModalIsOpen(false);
+  }
 
   useEffect(() => {
     if (!usuarioEstaLogado()) navigate("/painel");
@@ -24,10 +33,20 @@ export function App() {
       <div className="cabecalho-visita">
         <h2 className="titulo-visita">Visitas</h2>
 
-        <button className="btn-visita">
+        <button className="btn-visita" onClick={abrirModal}>
           <BsPlus /> criar nova visita
         </button>
       </div>
+
+      {modalIsOpen && (
+        <div className="container-modal">
+          <div className="modal">
+            <h2>Cadastro visita</h2>
+
+            <BiX className="close-icon" onClick={fecharModal} />
+          </div>
+        </div>
+      )}
     </main>
   );
 }
