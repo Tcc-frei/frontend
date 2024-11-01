e4c254ff14ba75e44f21dad68c240d9bd3da261f
Bruno O. <brunodeoliveira.22.10@gmail.com>
2024-11-01 13:06:54 -0300

    ok

--- src/service/axios.js
+++ src/service/axios.js
@@ -1,6 +1,11 @@
 import axios from "axios";
 
+// export const api = axios.create({
+//   baseURL: "http://localhost:3001",
+// });
+
+
 export const api = axios.create({
-  baseURL: "http://localhost:3001",
+  baseURL: "http://4.172.207.208:5010",
 });
 
